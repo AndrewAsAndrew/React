f3739d4537c94fb3a4de39b7ea8a9283629bbbf9
AndrewAsAndrew <yareyaredazzze@gmail.com>
2022-04-05 21:07:39 +0300

    Counters

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, {useState} from 'react'
+import Counter from "./components/Counter"
 
 function App() {
-  //деструктурируем аргументы функции useState (значение, функция)
-  const [likes, setLikes] = useState(0) //useState() - в реальном времени позволяет вносить изменения на странице приложения
   const [value, setValue] = useState('сменить текст')
-
-  const increment = () => {
-    setLikes(likes + 1)
-  }
-  const decrement = () => {
-    setLikes(likes - 1)
-  }
-
+  
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-
-     <input
-      type="text" 
-      value={value} 
-      onChange={event => setValue(event.target.value)}
-      />
-     <button onClick={increment}>Лайк)</button>
-     <button onClick={decrement}>Дизлайк(</button>
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
     </div>
   );
 }
