ba97437daa6fad6a53fbed0d5523f3e47950e098
AndrewAsAndrew <yareyaredazzze@gmail.com>
2022-04-05 19:48:57 +0300

    ReactLearn1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  //деструктурируем аргументы функции useState (значение, функция)
+  const [likes, setLikes] = useState(0) //useState() - в реальном времени позволяет вносить изменения на странице приложения
+  const [value, setValue] = useState('сменить текст')
+
+  const increment = () => {
+    setLikes(likes + 1)
+  }
+  const decrement = () => {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+
+     <input
+      type="text" 
+      value={value} 
+      onChange={event => setValue(event.target.value)}
+      />
+     <button onClick={increment}>Лайк)</button>
+     <button onClick={decrement}>Дизлайк(</button>
     </div>
   );
 }
